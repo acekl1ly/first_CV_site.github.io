8355b20f4274c1c161319f95d4c44949c9cbae3f
AbdurasulDj <abdurasulac@gmail.com>
2022-01-05 10:52:45 +0500

    commit before final changing circleBar.js

--- js/circleBar.js
+++ js/circleBar.js
@@ -1,5 +1,14 @@
 let progressBar = document.querySelectorAll(".circle-bar__progress");
 
+let skills = document.getElementById('skills');
+
+// const elementInView =  (element,  scrollOffset = 0) => {
+//     const elementTop = element.getBoundingClientRect().top;
+//     return (
+//         elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+//     );
+// };
+
 let progressStartValue = 0;
 let progressEndValues = [90,75,68,65,58,60,57,57,65];
 let speed = 60;
@@ -11,14 +20,69 @@ function setStyle(progressBar,progressStartValue,progressEndValues) {
         #4d5bf9 ${progressStartValue * 3.6}deg,
         #ffffff ${progressStartValue * 3.6}deg
     )`
-    console.log('ok');
     if (progressStartValue === progressEndValues) {
         clearInterval(progress)
         }
     },speed);
 };
 
-for ( var i = 0; i < progressBar.length; i++ ) {
-    // console.log(progressBar[i])
-    setStyle(progressBar[i],progressStartValue,progressEndValues[i])
-}
+// for ( var i = 0; i < progressBar.length; i++ ) {
+//     setStyle(progressBar[i],progressStartValue,progressEndValues[i])
+// }
+
+// const handleScrollAnimation = () => {
+//     progressBar.forEach((el) => {
+//       if (elementInView(el, 0)) {
+//         for ( var i = 0; i < progressBar.length; i++ ) {
+//             setStyle(progressBar[i],progressStartValue,progressEndValues[i])
+//          }
+//         return scrolled = false;
+//       };
+//     })
+//   }
+
+ 
+
+// const handleScrollAnimation = () => {
+// for ( var i = 0; i < progressBar.length; i++ ) {
+//     if (elementInView(progressBar[i], 0)) {
+//         setStyle(progressBar[i],progressStartValue,progressEndValues[i])
+//             console.log('ok');
+//         }
+
+//     }
+//   }
+
+
+let scrolled = true;
+
+// window.addEventListener('scroll', () => {
+//     if (elementInView(skills, 0)) {
+//         console.log('ok')
+//     }
+// })
+
+
+
+
+
+
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting || scrolled) {
+        console.log("ok");
+        for ( var i = 0; i < progressBar.length; i++ ) {
+            setStyle(progressBar[i],progressStartValue,progressEndValues[i])
+        }
+
+        scrolled = false;
+      return;
+    }
+
+    console.log('not ok');
+  });
+});
+
+observer.observe(skills);
